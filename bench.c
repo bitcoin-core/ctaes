57b961a9ebd1e084cb43e109550039d5b8667fb3
William Bright <wbright@protonmail.com>
2021-05-13 12:37:52 -0400

    added gettimeofday definition for windows port

--- bench.c
+++ bench.c
@@ -5,9 +5,34 @@
  **********************************************************************/
 #include <stdio.h>
 #include <math.h>
-#include "sys/time.h"
 
 #include "ctaes.h"
+
+#ifdef _WIN32
+#include <WinSock2.h>
+#include <stdint.h>
+
+/* gettimeofday is not availabe in windows so define here */
+int gettimeofday(struct timeval * tp, struct timezone * tzp) {
+    static const uint64_t EPOCH = ((uint64_t) 116444736000000000ULL);
+    SYSTEMTIME system_time;
+    FILETIME file_time;
+    uint64_t time;
+
+    GetSystemTime(&system_time);
+    SystemTimeToFileTime(&system_time, &file_time);
+    time = ((uint64_t)file_time.dwLowDateTime);
+    time += ((uint64_t)file_time.dwHighDateTime) << 32;
+
+    tp->tv_sec  = (long) ((time - EPOCH) / 10000000L);
+    tp->tv_usec = (long) (system_time.wMilliseconds * 1000);
+    return 0;
+}
+
+#else
+#include "sys/time.h"
+#endif
+
 
 static double gettimedouble(void) {
     struct timeval tv;
